b4728d6e76023b1be2518609a35912c570b46cbf
Itorius <Itorius@users.noreply.github.com>
2018-05-14 21:00:13 +0200

    Oil & Gas generation

---
+++ Global/PWorld.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Terraria;
+using Terraria.GameContent.Generation;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using Terraria.World.Generation;
+using TheOneLibrary.Base;
+using TheOneLibrary.Utils;
+
+namespace Potentia.Global
+{
+	public class PWorld : ModWorld
+	{
+		[Null] public static PWorld Instance;
+
+		public int[,] oil;
+		public int[,] gas;
+
+		public float RandomWeighted(float min, float max, float cap, int percent) => Main.rand.Next(0, 101) > percent ? Main.rand.NextFloat(min, cap) : Main.rand.NextFloat(cap, max);
+
+		public override void Initialize()
+		{
+			Instance = this;
+		}
+
+		public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+		{
+			oil = new int[Main.maxTilesX / 50, Main.maxTilesY / 50];
+			gas = new int[Main.maxTilesX / 50, Main.maxTilesY / 50];
+
+			tasks.Add(new PassLegacy("Potentia:OilGas", progress =>
+			{
+				progress.Message = "Fossilising dinosaurs";
+
+				for (int i = 0; i < oil.GetLength(0); i++)
+				{
+					for (int j = 0; j < oil.GetLength(1); j++)
+					{
+						oil[i, j] = (int)(1E6 * RandomWeighted(0.3f, 1.8f, 1f, 25));
+						gas[i, j] = (int)(1E6 * RandomWeighted(0.5f, 2.5f, 1.6f, 25));
+					}
+				}
+			}));
+		}
+
+		public override TagCompound Save()
+		{
+			TagCompound tag = new TagCompound();
+			tag["Width"] = oil.GetLength(0);
+			tag["Height"] = oil.GetLength(1);
+			tag["DataOil"] = oil.Cast<int>().ToList();
+			tag["DataGas"] = gas.Cast<int>().ToList();
+
+			return tag;
+		}
+
+		public override void Load(TagCompound tag)
+		{
+			int width = tag.GetInt("Width");
+			int height = tag.GetInt("Height");
+
+			oil = tag.GetList<int>("DataOil").To2DArray(width, height);
+			gas = tag.GetList<int>("DataGas").To2DArray(width, height);
+		}
+
+		public override void NetSend(BinaryWriter writer) => TagIO.Write(Save(), writer);
+
+		public override void NetReceive(BinaryReader reader) => Load(TagIO.Read(reader));
+	}
+}
--- Potentia.cs
+++ Potentia.cs
@@ -1,9 +1,71 @@
-﻿using Terraria.ModLoader;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Potentia.Global;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.UI;
+using TheOneLibrary.Base.UI;
+using TheOneLibrary.Base.UI.Elements;
+using TheOneLibrary.Utils;
 
 namespace Potentia
 {
 	public class Potentia : Mod
 	{
+		public GUI<TestUI> ui;
 
+		public override void Load()
+		{
+			ui = Utility.SetupGUI<TestUI>();
+		}
+
+		public override void Unload()
+		{
+			Utility.UnloadNullableTypes();
+		}
+
+		public override void PostUpdateInput()
+		{
+			if (Keys.X.IsKeyDown())
+			{
+				ui.@interface.IsVisible = !ui.@interface.IsVisible;
+				if (ui.@interface.IsVisible) ui.ui.Load();
+			}
+		}
+
+		public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+		{
+			layers.Add(ui.InterfaceLayer);
+		}
+	}
+
+	public class TestUI : BaseUI
+	{
+		public override void Load()
+		{
+			RemoveAllChildren();
+			
+			Texture2D texture = new Texture2D(Main.instance.GraphicsDevice, PWorld.Instance.oil.GetLength(0), PWorld.Instance.oil.GetLength(1));
+			Color[,] colors = new Color[texture.Width, texture.Height];
+			Main.NewText(PWorld.Instance.oil.Cast<int>().Count(x=>x>1E6));
+			for (int i = 0; i < texture.Width; i++)
+			{
+				for (int j = 0; j < texture.Height; j++)
+				{
+					colors[i, j] = Utility.DoubleLerp(Color.Red, Color.Yellow, Color.Lime, PWorld.Instance.oil[i, j] / (float)PWorld.Instance.oil.Cast<int>().Max());
+				}
+			}
+
+			texture.SetData(colors.Cast<Color>().ToArray());
+
+			UITexture t = new UITexture(texture);
+			t.Width.Set(texture.Width*10, 0);
+			t.Height.Set(texture.Height*10, 0);
+			t.Center();
+			Append(t);
+		}
 	}
 }
