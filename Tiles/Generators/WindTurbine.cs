bea2ae11f2ab8bafc871b0c1de184e2363375d79
Itorius <Itorius@users.noreply.github.com>
2018-06-10 11:54:06 +0200

    Force space between turbines

--- Tiles/Generators/WindTurbine.cs
+++ Tiles/Generators/WindTurbine.cs
@@ -29,6 +29,7 @@ namespace Potentia.Tiles.Generators
 			TileObjectData.newTile.CoordinateHeights = new[] { 16, 16, 16, 16, 16, 16 };
 			TileObjectData.newTile.CoordinateWidth = 16;
 			TileObjectData.newTile.CoordinatePadding = 2;
+			TileObjectData.newTile.HookCheck = new PlacementHook(CanPlace, -1, 0, true);
 			TileObjectData.newTile.HookPostPlaceMyPlayer = new PlacementHook(mod.GetTileEntity<TEWindTurbine>().Hook_AfterPlacement, -1, 0, false);
 			TileObjectData.addTile(Type);
 			disableSmartCursor = true;
@@ -38,6 +39,15 @@ namespace Potentia.Tiles.Generators
 			AddMapEntry(Color.LightYellow, name);
 		}
 
+		public int CanPlace(int i, int j, int type, int style, int direction)
+		{
+			for (int k = i - 4; k <= i + 4; k++)
+			{
+				if (Main.tile[k, j].type == mod.TileType<WindTurbine>() && k != i) return -1;
+			}
+			return 0;
+		}
+		
 		public override void RightClick(int i, int j)
 		{
 			TEWindTurbine panel = mod.GetTileEntity<TEWindTurbine>(i, j);
